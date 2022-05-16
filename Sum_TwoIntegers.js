24021c14003fe0d90b4d87ad07cb54a69422f459
hongsoom <hongsoom@kakao.com>
2022-05-16 20:31:25 +0900

    두 정수 사이의 합

---
+++ Sum_TwoIntegers.js
@@ -0,0 +1,14 @@
+function solution(a, b) {
+    let answer = 0;
+    let tmp = 0;
+    if (a > b) { 
+        tmp = a;
+        a = b;
+        b = tmp;
+    }
+    for (let i = a; i <= b; i++){
+        answer += i;
+    }
+    
+    return answer;
+}
