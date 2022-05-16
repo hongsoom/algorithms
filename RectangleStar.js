1c276483ef4c2db70d6bc1d0081af0e046a0a77c
hongsoom <hongsoom@kakao.com>
2022-05-16 20:02:54 +0900

    짝수와 홀수

--- RectangleStar.js
+++ RectangleStar.js
@@ -2,7 +2,7 @@ process.stdin.setEncoding('utf8'); // 표준입력으로 들어온 데이트를 
 process.stdin.on('data', data => { // 사용자가 키보드에서 Enter키를 눌렀을 때 발생하는 데이터 수신
     const n = data.split(" "); // 띄어쓰기 구분자로 여러개의 문자열을 배열로 나눔
     const a = Number(n[0]); // 문자 -> 숫자
-    const b = Number(n[1]); // 문자 ->
+    const b = Number(n[1]); 
     for(let i=1; i<=b; i++){ // 전체 반복되는 횟수 (세로)
         let str = ""
       for(let j=1; j<=a; j++){ // 찍어야되는 별의 수 만큼 반복(가로)
